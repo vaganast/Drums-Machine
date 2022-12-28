8af0509f2f6dbbcd913cef25a6c649ff0e3122fe
vaganast <71605687+vaganast@users.noreply.github.com>
2022-12-28 21:46:52 +0200

    finish project

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+const [pressedKey, setPressedKey] = useState('')
+
+
+  useEffect(() => { 
+    document.addEventListener('keydown', (event) => {
+      playMusic(event.key.toUpperCase())      
+    })   
+  }, [])
+
+  const pads = [
+    {
+      keyCode: 81,
+      text: "Q",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+    },
+    {
+      keyCode: 87,
+      text: "W",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+    },
+    {
+      keyCode: 69,
+      text: "E",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+    },
+    {
+      keyCode: 65,
+      text: "A",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+    },
+    {
+      keyCode: 83,
+      text: "S",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+    },
+    {
+      keyCode: 68,
+      text: "D",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+    },
+    {
+      keyCode: 90,
+      text: "Z",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+    },
+    {
+      keyCode: 88,
+      text: "X",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+    },
+    {
+      keyCode: 67,
+      text: "C",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+    }
+  ]
+
+const playMusic = (music) => {  
+    const audio = document.getElementById(music).play()  
+    setPressedKey(music)      
+    setTimeout(() => {
+      setPressedKey('')
+    },3000)  
+} 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="drum-machine" id="drum-machine">
+      <div id="display"><p>Lets play drums you pressed {pressedKey}</p></div>
+        <div className='drum-pads'>
+          {pads.map((pad) => 
+          <div className='drum-pad' 
+                tabIndex={0}            
+                key={pad.keyCode} 
+                id={pad.src} 
+                onClick={()=>{playMusic(pad.text)}}
+                >
+            {pad.text}
+            <audio  
+            className='clip' 
+            id={pad.text} 
+            src={pad.src} 
+            key={pad.keyCode}             
+            />
+        </div>)}
+      </div>      
     </div>
   )
 }
